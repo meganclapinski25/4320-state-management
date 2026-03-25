851a41d8003f20a2de1d8b6580674e7736d4ea42
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-25 13:29:38 -0700

    starting to work on form

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,35 @@ function App() {
   const[formData, setFormData] = useState({title: '', status:'todo'})
   console.log(tasks)
 
+  const handleSubmit =(e) =>{
+    e.preventDefault()
+  }
+
   return (
     <div>
       <h1>Tasks Tracker</h1>
       
       <section>
           <h2> Add a Task</h2>
-          <p>Form will go here</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder='Task Title'
+              value={formData.title}
+              onChange={(e)=>setFormData({...formData,title:e.target.value})}
+            />
+            <select
+              value={formData.status}
+              onChange={(e) => setFormData({...formData,status:e.target.value})}
+            >
+              <option value="todo">Todo</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
+              
+
+
+          </form>
       </section>
 
       <section> 
