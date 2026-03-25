c4582fdb61b9a14ec7928e47d388d1cbf106dd04
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-25 13:24:08 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
   const [tasks, setTasks] = useState([
-    'id',
-    'to-do',
+    {
+      id: '1',
+      title: 'Test Task',
+      status: 'todo',
+    }
+
   ])
+
+  const[formData, setFormData] = useState({title: '', status:'todo'})
   console.log(tasks)
 
   return (
@@ -25,7 +28,7 @@ function App() {
       </section>
 
       <section> 
-        <h2> Tasks (0) </h2>
+        <h2> Tasks (tasks.length) </h2>
         <p>List of tasks will go here </p>
       </section>
     </div>
