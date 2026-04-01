960715d742502157e4e8b0538815530877b6de98
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-04-01 13:50:19 -0700

    starting adding prop and useNav variable

--- src/pages/TaskNew.jsx
+++ src/pages/TaskNew.jsx
@@ -1,2 +1,15 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+
+function TaskNew({onCreate}){
+
+
+    const navigate = useNavigate()
+    
+
+    return(
+        
+    )
+}
+
+export default TaskNew
