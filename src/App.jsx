a920d8f85fe25a1020e72b0fb575693d5a3b24d9
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-04-01 13:08:52 -0700

    fixed main and started working on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,75 +7,13 @@ import TaskForm from './components/TaskForm'
 function App() {
   
   const [tasks, setTasks] = useState([])
-  const[formData, setFormData] = useState({title: '', status:'todo'})
-  const [editingId, setEditingId] = useState(null)
-
-  console.log(tasks)
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (editingId) {
-      const updatedTasks = tasks.map((task) => {
-        if (task.id === editingId) {
-          return { ...task, title: formData.title, status: formData.status }
-        }
-        return task
-      })
   
-      setTasks(updatedTasks)
-      setEditingId(null)
-    }else{
-      const newTask = {
-        id: crypto.randomUUID(),
-        title: formData.title,
-        status: formData.status,
-      }
-      setTasks([...tasks, newTask])
-    }
-    
-
-    setFormData({ title: '', status: 'todo' })
-  }
-
-  const handleDelete = (id) =>{
-    setTasks(tasks.filter(task => task.id !== id))
-  }
-
-  const handleEdit = (task) => {
-    setEditingId(task.id)
-    setFormData({ title: task.title, status: task.status })
-  }
-
-  const handleCancel = () => {
-    setEditingId(null)
-    setFormData({title: '', status: 'todo'})
-  }
-
-  const compCount = tasks.filter(task => task.status === 'done').length
 
   return (
-    <div>
-      <TaskForm
-        formData={formData}
-        setFormData={setFormData}
-        handleSubmit={handleSubmit}
-        editingId={editingId}
-        onCancel={handleCancel}
-      />
-
-      <section> 
-        <h2> Tasks ({tasks.length}) </h2>
-        {tasks.map(task => (
-          <TaskItem
-            key={task.id}
-            task={task}
-            onDelete={handleDelete}
-            onEdit={handleEdit}
-          />
-        ))}
-      </section>
-      <p>{compCount} of {tasks.length} tasks completed</p>
-    </div>
+   <Routes>
+    <Route path ="/" element={<Navigate to="/tasks" replace/> }/>
+    <Route path="/tasks" element={<TaskList tasks={tasks} onDelete={deleteTask} />} />
+   </Routes>
   )
   
 }
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,17 @@
 import { Link } from 'react-router-dom'
 import TaskItem from '../components/TaskItem'
 
+function TaskList({tasks, onDelete}) {
 
+  return(
+    <div>
+       <h2>Task List</h2>
+       <h2>Amount of Tasks {tasks.length}</h2>
+       <Link to= "/tasks/new">New Task</Link>
+       {tasks.map(task => (
+          <TaskItem key={task.id} task={task} onDelete={onDelete}/>
+        ))}
+    </div>
+   
+  )
+}
