2432b3783f3beb7a828f4d4de00ad0978fb3f50a
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-04-15 13:23:34 -0700

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import TaskList from './pages/TaskList'
@@ -8,26 +8,12 @@ import TaskDetail from './pages/TaskDetail'
 
 function App() {
   
-  const [tasks, setTasks] = useState([])
-  
-  const deleteTask = (id) =>{
-    setTasks(tasks.filter(task =>task.id !==id))
-  }
-
-  const addTask = (task) =>{
-    setTasks([...tasks,task])
-  }
-
-  const updateTask = (updatedTask)=>{
-    setTasks(tasks.map(task => task.id === updatedTask.id ? updatedTask: task))
-  }
-
   return (
    <Routes>
     <Route path ="/" element={<Navigate to="/tasks" replace/> }/>
-    <Route path="/tasks" element={<TaskList tasks={tasks} onDelete={deleteTask} />} />
-    <Route path="/tasks/new" element={<TaskNew onCreate={addTask} />} />
-    <Route path="/tasks/:id" element={<TaskDetail tasks={tasks} onUpdate={updateTask} onDelete={deleteTask}/>}/>
+    <Route path="/tasks" element={<TaskList />} />
+    <Route path="/tasks/new" element={<TaskNew />} />
+    <Route path="/tasks/:id" element={<TaskDetail/>}/>
    </Routes>
   )
   
