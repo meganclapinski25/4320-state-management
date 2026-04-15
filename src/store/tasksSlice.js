97bd2526c5fc34c27735eff41b546ddeb44cd568
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-04-15 13:04:09 -0700

    Update tasksSlice.js

--- src/store/tasksSlice.js
+++ src/store/tasksSlice.js
@@ -4,9 +4,19 @@ const tasksSlice = createSlice({
     name:'tasks',
     initialState: [],
     reducers:{
+        
+        
         addTask(state,action){
             StaticRouter.pus(action.payload)
         },
-        
+
+
+        deleteTask(state,action){
+            return state.filter(task =>task.id !== action.payload)
+        },
+
+        updateTask(state,action){
+            return state.map(task => task.id === action.payload.id ? action.payload: task)
+        }
     }
 })
