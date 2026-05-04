85326dd7ebb455852214e3c98d4ed4fcbf047a6a
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-05-04 11:58:04 -0700

    Update TaskItem.jsx

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,12 +1,26 @@
 import { Link } from 'react-router-dom'
-
+import { selectAllCategories } from '../store/categoriesSlice'
+import { useSelector } from 'react-redux'
 function TaskItem({ task, onDelete }) {
   const statusClass = `status-badge status-${task.status}`
 
+
+
+  const categories = useSelector(selectAllCategories)
+  const category = Array.isArray(categories)
+    ? categories.find(c => String(c.id) === String(task.categoryId))
+    : null
   return (
     <div className="task-item">
       <p className="task-title">{task.title}</p>
+      {category && (
+        <span className ="cat-tag" style= {{background:category.color}}>
+          {category.name}
+        </span>
+      )}
+
       <span className={statusClass}>{task.status}</span>
+
       <div className="task-actions-row">
         <button className="delete-btn" onClick={() => onDelete(task.id)}>Delete</button>
         <Link to={`/tasks/${task.id}`} className="edit-btn">Edit</Link>
