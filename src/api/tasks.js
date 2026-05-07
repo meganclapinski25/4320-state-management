2951b4d02584a9d31574a0f48c3e204bdc83197a
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-05-07 12:26:51 -0700

    starting async functions

---
+++ src/api/tasks.js
@@ -0,0 +1,17 @@
+const API_URL = 'http://localhost:3001/tasks'
+
+export async function getTasks(){
+
+}
+
+export async function createTask(task){
+
+}
+
+export async function updateTask(task){
+
+}
+
+export async function deleteTask(id){
+    
+}
