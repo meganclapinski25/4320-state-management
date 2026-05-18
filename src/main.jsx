f094aa50a34d9f100c1ad1943c3c573efedd28ed
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-05-18 10:50:14 -0700

    finsih

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import store from './store/index.js'
 import './index.css'
 import App from './App.jsx'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </Provider>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 )
